a00272ede52ed3f2028d9fe3d24ee2de4a76c316
christophettat <christophe_xxx@yahoo.fr>
2019-06-02 13:22:47 +0200

    fully working code

--- PythonHelpers/ConditionnalStart.py
+++ PythonHelpers/ConditionnalStart.py
@@ -1,20 +1,23 @@
 import os
 import time
 
-ROBOT_LISTENER_API_VERSION = 3
-class ResultModifier(object):
 
+
+class ConditionnalStart(object):
+    ROBOT_LISTENER_API_VERSION = 3
 
     def __init__(self, file='flag.txt'):
         self.flag = file
 
     def start_test(self, data, test):
-      # wait condition to start the test, this should typically be a call to a graphana alert.
-      while not os.path.isfile(self.flag):
-        time.sleep(20)
+        # wait condition to start the test, this should typically be a call to a graphana alert.
+        while not os.path.isfile(self.flag):
+            time.sleep(20)
+        print "************* Starting test exect\n"
 
     def end_test(self, data, test):
          # check if test conditions degraded druing the run, if so, add a tag to the tests results, this should typically be the same call as above
         if not os.path.isfile(self.flag):
-            test.tag.add("unstable")
+            test.tags.add("unstable")
             test.message = 'System became unavailable during the test'
+        print "************* TEST COMPLETE \n"
